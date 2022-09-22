7a60e280ebadfc234d297be1aa076cd17bcd2121
rkildea1 <31891933+rkildea1@users.noreply.github.com>
2022-09-22 19:39:54 +0100

    Update variables.py

--- app/variables.py
+++ app/variables.py
@@ -1,4 +1,4 @@
-
+import time
 import load_env as load_env_vars
 
 #referenced in all_advert_urls_scraper.py
@@ -6,10 +6,18 @@ import load_env as load_env_vars
 num_of_pages = 1 
 output_files_folder_name = 'output_files'
 all_links_csv_name = 'mycsvexport.csv' #what do you want to call the output csv which stores all hrefs (i.e., all the actual property advertisement links)
+csv_output_path = (output_files_folder_name+'/'+all_links_csv_name) #prints `output_files/mycsvexport.csv`
 
 
 
+timestamp = time.strftime("%Y%m%d-%H%M%S")
+json_file_path = (output_files_folder_name+'/'+'ad_details_raw')
+json_file_name = 'data.json'+timestamp
+json_output_path = (json_file_path+'/'+json_file_name)
 
+
+local_image_folder_path = (output_files_folder_name+'/'+'ad_details_photos/') #which directory on your machine should you store the photos 
+s3_photo_folder_path = 'ad_details_photos/' #which directory on s3 should you store the photos
 
 
 ##privateVariables
